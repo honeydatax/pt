8182b83a5c5ab3ee8d8ef6671c69740758248534
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-12-21 16:23:09 +0000

    convert a string to numero

---
+++ numero.vb
@@ -0,0 +1,9 @@
+imports pt
+module modelo1
+	sub main()
+		dim numero as double
+		numero=pt.pts.pi*3
+		pt.pts.escrever("o seu numero é .!")
+		pt.pts.escrever(numero.tostring())
+	end sub
+end module
