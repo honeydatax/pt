382f5cbf2105bae241903dc8d7f4728def367aa2
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-12-21 16:24:04 +0000

    converte um numero para texto

---
+++ numero.cs
@@ -0,0 +1,13 @@
+using pt;
+
+	public class ola{
+		public static void Main(string[] args){
+			double numero ;
+			numero=pt.pts.Pi*3;
+			pt.pts.escrever("o seu numero é.!");
+			pt.pts.escrever(numero.ToString());
+			
+			
+		}
+	}
+
