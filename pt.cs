463f239de377e7e3f3ec03a4f2b617c9147f3239
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-12-22 20:21:44 +0000

    ultima versão do pt.net

--- pt.cs
+++ pt.cs
@@ -3,6 +3,17 @@ using System;
 
 namespace pt{
 	public class pts{
+		public static int cor_papel = 0;
+		public static int cor_tinta = 7;
+		public static int preto = 0;
+		public static int vermelho = 1;
+		public static int verde = 2;
+		public static int laranja = 3;
+		public static int azul = 4;
+		public static int rosa = 5;
+		public static int cyan = 6;
+		public static int cinza = 7;
+		public static int branco = 7;
 		
 		public static double Pi = 3.1415927;
 		public static void escrever(String s){
@@ -38,6 +49,29 @@ namespace pt{
 			s="\x1b[" + sx + sy ;
 			Console.Write(s);
 		}
+		public static void papel(int valor){
+			string s;
+			string sx;
+			string sy;
+			cor_papel=valor;
+			sx=(cor_tinta+30).ToString() + ";" ;
+			sy=(cor_papel+40).ToString() + "m" ;
+			s="\x1b[" + sx + sy ;
+			Console.Write(s);
+
+		}
+		public static void tinta(int valor){
+			string s;
+			string sx;
+			string sy;
+			cor_tinta=valor;
+			sx=(cor_tinta+30).ToString() + ";" ;
+			sy=(cor_papel+40).ToString() + "m" ;
+			s="\x1b[" + sx + sy ;
+			Console.Write(s);
+
+		}
+
 	}
 	
 }
